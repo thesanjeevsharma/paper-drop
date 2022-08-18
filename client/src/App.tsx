c2071b4055d5da08bc25d4e9aa145986ae608e6a
thesanjeevsharma <29539278+thesanjeevsharma@users.noreply.github.com>
2022-08-18 18:30:44 +0530

    feat: add Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages";
+import { Home, Login } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ client/src/pages/Login/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { useNavigate } from "react-router-dom";
+
+import { Layout } from 'src/containers'
+
+import './Login.css'
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/', { replace: true })
+    }
+
+    return (
+        <Layout>
+            <Flex className="login-wrapper" direction="column" align="center" justify="center" h="100vh" w="full" color="white">
+                <Box mb={16} textAlign="center">
+                    <Text fontSize="4xl" fontWeight="bold">PaperDrop</Text>
+                    <Text fontSize="l">Drop secret messages across the world!</Text>
+                </Box>
+
+                <Button bg="white" color="green.700" onClick={handleLogin}>Login with Google</Button>
+            </Flex>
+        </Layout>
+    )
+}
+
+export default Login
