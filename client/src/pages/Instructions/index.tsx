b93b68c63776d3e3d9c46d581d960a84c37133ac
Sanjeev <29539278+thesanjeevsharma@users.noreply.github.com>
2022-08-27 00:29:52 +0530

    chore: update rules

--- client/src/pages/Instructions/index.tsx
+++ client/src/pages/Instructions/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { ChevronLeftIcon, EmailIcon } from '@chakra-ui/icons';
 import { Box, Flex, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
@@ -38,40 +38,45 @@ const Instructions = () => {
                </ListItem>
                <ListItem mb={1}>
                   <Text>
-                     As a user you have a choice to drop a message anonymously
-                     or with your name.
+                     As a user you have a choice to drop a message{' '}
+                     <b>anonymously</b> or with your name.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
                   <Text>
                      Nearby Drops: On this sections of the app, drops made by
-                     other users will be visible on the map within 2km of range.
+                     other users will be visible on the map within <b>2km</b> of
+                     range. All nearby drops will be indicated by{' '}
+                     <EmailIcon color="gray.600" /> icon.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
                   <Text>
                      My Drops: On this section of the app, you can see all the
                      drops made by you. You can also delete the drops by tapping
-                     on them.
+                     on them. These drops will be indicated by{' '}
+                     <EmailIcon color="green.400" /> icon.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
                   <Text>
-                     You can read messages dropped by other users within 50m
-                     range of your current location.
+                     You can read messages dropped by other users within{' '}
+                     <b>50m</b> range of your current location. These drops will
+                     be indicated by <EmailIcon color="purple.600" /> icon.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
                   <Text>
-                     A drop is removed from the map when it has been read by 10
-                     people(i.e. expired) or it has been deleted by the author.
+                     A drop is removed from the map when it has been read by{' '}
+                     <b>10 people</b>(i.e. expired) or it has been deleted by
+                     the author.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
                   <Text>
-                     At any given point a user can only have 10 drops active. To
-                     create new drops, either wait for older drops to expire or
-                     delete some.
+                     At any given point a user can only have{' '}
+                     <b>10 active drops</b>. To create new drops, either wait
+                     for older drops to expire or delete some.
                   </Text>
                </ListItem>
                <ListItem mb={1}>
